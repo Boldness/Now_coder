6b7612a1fac5b49c4276f92fa3a40c67612fbfcb
alpha008 <zjxlove2017@163.com>
2017-07-18 22:36:25 +0800

    pass

--- sort/sort/sort.cpp
+++ sort/sort/sort.cpp
@@ -7,7 +7,7 @@ class Solution
 {
 public:
 	void Insert_Sort(vector<int> &a, int num);
-	void Simple_Sort(int *a, int n);
+	void Simple_Sort(vector<int> &a, int n);
 	void Pop_Sort(int *a, int n);
 	void Qucik_Sort(int *a,int l,int r);
 	void Shell_Sort(int *a,int n);
@@ -29,14 +29,29 @@ void Solution::Insert_Sort(vector<int> &a, int num){
 	插入排序法，时间复杂度为n平方
 	从第二个元素开始，往后依次挖出来，然后和之前的元素进行比较，如果比其前面的元素小，
 	就和它互相交换位置，直到第1个元素为止
-
 */
+}
+void Solution::Simple_Sort(vector<int> &a, int n){
+	for (int i = 0; i < n; i++){
+		int key = a[i];
+		int index = i;//这里都是相对位置，起始地址
+		for (int j = i + 1; j < n; j++){
+			if (a[j] < key){
+				key = a[j];
+				index = j;
+			}
+		}
+			int tmp = a[i];
+			a[index] = tmp;
+			a[i] = key;
+	}
 }
 void main()
 {
 	vector<int> a = {44,66,22,45,78,98,34};
 	Solution aa;
-	aa.Insert_Sort(a,a.size());
+	//aa.Insert_Sort(a,a.size());
+	aa.Simple_Sort(a,a.size());
 	for (auto p : a){   //这里循环遍历容器内元素
 		cout << p << " ";
 	}
