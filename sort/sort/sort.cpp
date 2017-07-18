795d3a33c921a5c76d96b53763eefae7a0466bfe
alpha008 <zjxlove2017@163.com>
2017-07-18 21:37:09 +0800

    sort

---
+++ sort/sort/sort.cpp
@@ -0,0 +1,26 @@
+/*∞À÷÷≈≈–Ú∑Ω∑®*/
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Solution
+{
+public:
+	void Insert_Sort(int *a,int num);
+	void Simple_Sort(int *a, int n);
+	void Pop_Sort(int *a, int n);
+	void Qucik_Sort(int *a,int l,int r);
+	void Shell_Sort(int *a,int n);
+	void Heap_Sort(int *a,int n);
+	void Merge_Sort(int *a,int l,int mid,int r);
+	void Isdradix_sort(int *arr,int begin,int end);
+};
+void Solution::Insert_Sort(int *a, int num){
+
+}
+
+void main()
+{
+	cout << "hello" << endl;
+	system("pause");
+}
